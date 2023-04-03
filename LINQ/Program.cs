3cd86f18785c52d3ea1f088048f0ce95272203c5
Nina <boldurfaith@gmail.com>
2023-04-03 06:15:13 +0100

    initial commit

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGamesList = new List<string>
+           {
+               "Mass Effect",
+               "KOTOR",
+               "Mortal Combat",
+               "Halo",
+               "Bricks"
+            };
+
+            Console.WriteLine("Original List:");
+            videoGamesList.ForEach(Console.WriteLine);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Ordered List:");
+            var gamesListOrdered = videoGamesList.OrderBy(name => name.Length);
+
+            foreach(var gameName in gamesListOrdered)
+            {
+                Console.WriteLine(gameName);
+            }
+
+            //Alternatively, one can rewrite the ordered list like this below:
+            //Console.WriteLine("Ordered List:");
+            //videoGamesList.OrderBy(name => name.Length).ToList().ForEach(Console.WriteLine);
+
         }
     }
 }
